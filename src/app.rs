f4af9eab65aa6ce83531878eabbb29800ddc28e2
mageOfStructs <jason.puschnig@gmx.at>
2024-10-15 12:23:20 +0200

    add nav demo from docs

--- src/app.rs
+++ src/app.rs
@@ -19,11 +19,47 @@ use thaw::*;
 #[component]
 pub fn App() -> impl IntoView {
     view! {
-        <NavDrawer>
-            <NavItem icon=icondata::AiChromeOutlined value="chrome">
-                <Button>"Chrome"</Button>
-            </NavItem>
-        </NavDrawer>
+            <NavDrawer>
+        <NavCategory value="area">
+            <NavCategoryItem slot icon=icondata::AiAreaChartOutlined>
+                "Area Chart"
+            </NavCategoryItem>
+            <NavSubItem value="target">
+                "Target"
+            </NavSubItem>
+            <NavSubItem value="above">
+                "Above"
+            </NavSubItem>
+            <NavSubItem value="below">
+                "Below"
+            </NavSubItem>
+        </NavCategory>
+        <NavCategory value="pie">
+            <NavCategoryItem slot icon=icondata::AiPieChartOutlined>
+                "Pie Chart"
+            </NavCategoryItem>
+            <NavSubItem value="pie-target">
+                "Pie Target"
+            </NavSubItem>
+            <NavSubItem value="pin-above">
+                "Pin Above"
+            </NavSubItem>
+            <NavSubItem value="pin-below">
+                "Pin Below"
+            </NavSubItem>
+        </NavCategory>
+        <NavItem
+            icon=icondata::AiGithubOutlined
+            value="github"
+            href="https://github.com/thaw-ui/thaw"
+            attr:target="_blank"
+        >
+            "Github"
+        </NavItem>
+        <NavItem icon=icondata::AiChromeOutlined value="chrome">
+            "Chrome"
+        </NavItem>
+    </NavDrawer>
     }
     // let (name, set_name) = create_signal(String::new());
     // let (greet_msg, set_greet_msg) = create_signal(String::new());
